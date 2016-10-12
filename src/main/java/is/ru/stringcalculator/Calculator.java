dbe17c0c7dac7fb5a6d0ed49b82c5b1c5e7a70ef
Glitch <runylh@gmail.com>
2016-10-12 00:58:59 +0000

    Added support for a single number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,9 @@ package is.ru.stringcalculator;
 public class Calculator {
     public static int add(String numbers)
     {
-        return 0;
+        if(numbers.isEmpty())
+            return 0;
+
+        return Integer.parseInt(numbers);
     }
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -11,5 +11,11 @@ public class CalculatorTest {
         assertEquals(0, Calculator.add(""));
     }
 
+    @Test
+    public void testOneNumber()
+    {
+        assertEquals(1, Calculator.add("1"));
+    }
+
 }
 
