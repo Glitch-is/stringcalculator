7a1192c49e47e5a2eee0622d0e5f053081ba1758
Glitch <runylh@gmail.com>
2016-10-12 00:48:43 +0000

    Added the given skeleton

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,5 @@
+package is.ru.stringcalculator;
+
+public class Calculator {
+
+}
---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,9 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+}
+
